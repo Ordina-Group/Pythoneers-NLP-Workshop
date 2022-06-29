6cbd2afcd7b8920c26147c838917d788ef8398d5
J.F.P. (Richard) Scholtens <richardscholtens2@gmail.com>
2022-06-29 18:27:29 +0200

    Used TfidfVectorizer with LogisticRegression

--- nlp_model/classifier.py
+++ nlp_model/classifier.py
@@ -12,6 +12,8 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
 
 
 def main():
@@ -19,7 +21,7 @@ def main():
     output_model_path = Path.cwd() / "nlp_model/clf.pickle"
 
     df = pd.read_csv(input_data_path, sep='\t', names=['SENTENCE_NR', 'WORD', 'POS', 'POS_TAG', 'NER_TAG'])
-    
+    # df = pd.read_csv('/data/train.csv', sep='\t', names=['SENTENCE_NR', 'WORD', 'POS', 'POS_TAG', 'NER_TAG'])
     # df = df[["SENTENCE_NR", "WORD"]]
 
     # Adjust to allow more data to be trained
@@ -29,33 +31,40 @@ def main():
 
     df.dropna(inplace=True)
 
-    # vectorizer = DictVectorizer(sparse=False)
-    # x = vectorizer.fit_transform(df.to_dict("records"))
-    vectorizer = TfidfVectorizer(max_df=0.5, min_df=0, stop_words="english")
-    x = vectorizer.fit_transform(df["WORD"])
+    cls = LogisticRegression()
+    vec = TfidfVectorizer()
 
-    vectorizer = CountVectorizer(stop_words="english")
-    x_new = vectorizer.fit(df["WORD"])
+    classifier = Pipeline([("vec", vec), ("cls", cls)])
 
-    y = df.NER_TAG.values
-    all_classes = np.unique(y)
+
+    # x = df[["SENTENCE_NR", "WORD", "POS", "POS_TAG"]].tolist()
+    x = df["WORD"].tolist()
+    x = vec.fit_transform(x).toarray()
+
+    y = df["NER_TAG"]
+
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-    # clf = Perceptron(verbose=10, n_jobs=-1)
-    # clf.partial_fit(x_train, y_train, all_classes)
 
-    # print(classification_report(y_test, clf.predict(x_test)))
-    # print(f1_score(clf.predict(x_test), y_test, average="micro"))
+    print("shape")
+    print(x.shape)
+    print(y.shape)
 
-    # pickle.dump(clf, open(output_model_path, "wb"))
+    print(x_train.shape)
+    print(y_train.shape)
 
-    log_reg = LogisticRegression()
-    log_reg.fit(x_train, y_train)
 
-    print(classification_report(y_test, log_reg.predict(x_test)))
 
-    pickle.dump(log_reg, open(output_model_path, "wb"))
+    print("going to fit ")
+    print(y_train)
+
+    cls.fit(x_train, y_train)
+    y_pred = cls.predict(x_test)
+    print(y_pred)
+    print(classification_report(y_test, cls.predict(x_test)))
+
+    pickle.dump(cls, open(output_model_path, "wb"))
 
 
 if __name__ == '__main__':
