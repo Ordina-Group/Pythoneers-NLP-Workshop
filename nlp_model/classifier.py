40b47585a7a1386309651f6b4618aa9b1a4d5913
J.F.P. (Richard) Scholtens <richardscholtens2@gmail.com>
2022-07-04 15:08:09 +0200

    removed unneeded code

--- nlp_model/classifier.py
+++ nlp_model/classifier.py
@@ -4,14 +4,12 @@
 import pickle
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 
 
 def main():
