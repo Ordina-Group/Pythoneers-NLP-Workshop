b03daeeafec0830e560a7968317a6ebcd36271b9
marielleboot <82645719+marielleboot@users.noreply.github.com>
2022-07-05 14:27:20 +0200

    Add `README.md`
    Minor changes in `classifier.py` and `requester.py`

--- requester.py
+++ requester.py
@@ -2,6 +2,8 @@ import requests
 
 
 if __name__ == "__main__":
+    # Every time you run this module, it will add the file(s) in the dict below
+    # to the database instance.
     files = {"file": open("file.txt", "rb")}
     r = requests.post("http://localhost:8000/upload_file", files=files)
     print(r.content)
