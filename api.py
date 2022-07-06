ba1a335b67dbd5d063c3c3edbf0e9238361b42d7
Gijs <g.lakeman@student.rug.nl>
2022-07-06 15:04:17 +0200

    Gezellie zonder Caselli

--- api.py
+++ api.py
@@ -1,4 +1,13 @@
+import re
+import pickle
+from pathlib import Path
+from collections import defaultdict
+import pandas as pd
+
 from fastapi import FastAPI, UploadFile
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import string
 
 from db import (
     add_entry,
@@ -31,22 +40,29 @@ async def upload_file(file: UploadFile) -> dict:
     finally:
         await file.close()
 
-    return ...
+    return {"message": message, "file_name":file_name}
 
 
 @app.get("/file")
 def get_file() -> dict:
-    ...
+    files = get_all_entries(TABLE_NAME)
+    files = [{'id': f[0], 'file_name': f[1]} for f in files]
+    return {"nr_files":len(files), "files": files}
 
 
 @app.get("/file/{file_id}")
 def get_file(file_id: int) -> dict:
-    ...
+    file = get_entry_by_id(TABLE_NAME, file_id)
+    return {"file_name": file[0], "contents": file[1]}
 
 
 @app.get("/file/{file_id}/words")
 def get_words(file_id: int) -> dict:
-    ...
+    file = get_entry_by_id(TABLE_NAME, file_id)
+    translator = str.maketrans('', '', string.punctuation)
+    words = file[1].translate(translator)
+    words = word_tokenize(words)
+    return {"word_count": len(words), "unique_words": set(words)}
 
 
 @app.get("/file/{file_id}/letters")
@@ -56,9 +72,47 @@ def get_letters(file_id: int) -> dict:
 
 @app.get("/file/{file_id}/sentiment")
 def get_sentiment(file_id: int) -> dict:
-    ...
+    analyzer = SentimentIntensityAnalyzer()
+
+    file = get_entry_by_id(TABLE_NAME, file_id)
+    sentences = sent_tokenize(file[1])
+    sentiments = []
+    for sentence in sentences:
+        polarity =  analyzer.polarity_scores(sentence)
+        sentiments.append(polarity)
+    return {"sentiment": sentiments}
+
 
 
 @app.get("/file/{file_id}/named_entities")
 def get_named_entities(file_id: int) -> dict:
-    ...
+    _, contents = get_entry_by_id(TABLE_NAME, file_id)
+
+    sentences = sent_tokenize(contents)
+
+    data = []
+    for sentence in sentences:
+        print(sentence)
+        words = word_tokenize(sentence)
+        data.extend(words)
+
+    df = pd.DataFrame(data, columns=["WORD"])
+
+    input_array = df["WORD"].tolist()
+
+    vec = pickle.load(open("nlp_model/tfidf_vec.pickle", 'rb'))
+
+    input_vector = vec.transform(input_array)
+
+
+    model_path = Path.cwd() / "nlp_model/clf.pickle"
+
+    cls = pickle.load(open(model_path, "rb"))
+
+    ner_tag = cls.predict(input_vector)
+
+    named_entities = [(word, ner_tag) for word, ner_tag in zip(input_array, ner_tag)]
+
+    dict = defaultdict(list)
+    dict['named_entities'] = named_entities
+    return dict
--- nlp_model/classifier.py
+++ nlp_model/classifier.py
@@ -1,6 +1,42 @@
-def main():
-    ...  # Write your code here
+import pickle
+from pathlib import Path
 
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, accuracy_score
+import pandas as pd
+
+def main():
+    input_data_path = Path.cwd() / "data/train.csv"
+    output_model_path = Path.cwd() / "nlp_model/clf.pickle"
+    output_vectorizer_path = Path.cwd() / "nlp_model/tfidf_vec.pickle"
+
+    train = pd.read_csv(input_data_path, sep="\t", names=["SENTENCE_NR", "WORD", "POS", "POS_TAG", "NER_TAG"])
+    
+    train = train[:180000]
+    train.dropna(inplace=True)
+    
+    X = train["WORD"].tolist()
+    Y = train["NER_TAG"].tolist()
+
+    model = LinearSVC()
+    vec = TfidfVectorizer()
+
+    X = vec.fit_transform(X)
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+    model.fit(x_train, y_train)
+
+    y_pred = model.predict(x_test)
+    print("Confusion matrix")
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, model.predict(x_test)))
+    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+
+    pickle.dump(model, open(output_model_path, "wb"))
+    pickle.dump(vec, open(output_vectorizer_path, "wb"))
 
 if __name__ == '__main__':
     main()
