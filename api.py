f684562cc9e9f8ee3cfb1aa60be011fd07e5d5dc
wim-peter <wim-peter.dirks@ordina.nl>
2022-08-01 15:29:14 +0200

    Changes made by pink fluffy unicorns

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, UploadFile
-
+import nltk
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import spacy
 from db import (
     add_entry,
     get_all_entries,
@@ -31,22 +33,35 @@ async def upload_file(file: UploadFile) -> dict:
     finally:
         await file.close()
 
-    return ...
+    return message
 
 
 @app.get("/file")
 def get_file() -> dict:
-    ...
+    # {"nr_files":<number_of_files>,"files":[{"id":<id>,"file_name":"<file_name>"},...}]}
+    entries = get_all_entries(TABLE_NAME)
+    nr_files = len(entries)
+    files = []
+    for item in entries:
+        files.append({item[0]: item[1]})
+    file_dict = {'nr_files': nr_files, 'files':files}
+    return file_dict
+
 
 
 @app.get("/file/{file_id}")
 def get_file(file_id: int) -> dict:
-    ...
+    entry = get_entry_by_id(TABLE_NAME, file_id)
+    return {'file_name': entry[0], 'contents': entry[1]}
 
 
 @app.get("/file/{file_id}/words")
 def get_words(file_id: int) -> dict:
-    ...
+    entry = get_entry_by_id(TABLE_NAME, file_id)
+    text = entry[1]
+
+    tokens = nltk.tokenize.word_tokenize(text)
+    return {"word_count": len(tokens), "unique_words": set(tokens)}
 
 
 @app.get("/file/{file_id}/letters")
@@ -56,9 +71,22 @@ def get_letters(file_id: int) -> dict:
 
 @app.get("/file/{file_id}/sentiment")
 def get_sentiment(file_id: int) -> dict:
-    ...
+    entry = get_entry_by_id(TABLE_NAME, file_id)
+    analyzer = SentimentIntensityAnalyzer()
+    sentiment_dict = analyzer.polarity_scores(entry[1])
+    return {'sentiment': sentiment_dict}
 
 
 @app.get("/file/{file_id}/named_entities")
 def get_named_entities(file_id: int) -> dict:
-    ...
+    entry = get_entry_by_id(TABLE_NAME, file_id)
+    text = entry[1]
+    model = spacy.load("en_core_web_sm")
+    doc = model(text)
+    pos_list = []
+    for word in doc.ents:
+        pos_list.append([word.text, word.label_])
+    return {'named_entities': pos_list}
+
+
+
