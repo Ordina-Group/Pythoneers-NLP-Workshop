163d45a16df1f896a3c9e519bfeb62c41406a014
wim-peter <wim-peter.dirks@ordina.nl>
2023-02-02 14:44:48 +0100

    Fix SQL-injection vulnerability (partially)

--- db.py
+++ db.py
@@ -1,94 +1,76 @@
 import sqlite3
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 DB_NAME: str = "files.db"
 TABLE_NAME: str = "Files"
-ENTITIES: List[str] = ["File_name", "Contents"]
 DATA: List[Tuple[str, str]] = [("File_name", "TEXT"), ("Contents", "TEXT")]
 
 
-def sql_execute(sql_string: str) -> str:
-    """Execute provided SQL string on db, returns id of last row on cursor."""
+def sql_execute(
+    sql_string: str,
+    parameters: tuple = (),
+) -> str:
+    """Execute provided SQL string with parameters on db."""
     with sqlite3.connect(DB_NAME) as connection:
-        cursor = connection.cursor()
-        cursor.execute(sql_string)
-        connection.commit()
-    return cursor.lastrowid
+        cursor = connection.execute(sql_string, parameters)
+    return cursor.fetchall()
 
 
-def create_table(table_name: str, entities: List[Tuple[str, str]]) -> None:
+def create_table(entities: List[Tuple[str, str]]) -> None:
     """Create table if it does not exist already.
 
     :entities: List of tuples containing (entity_name, entity_format), e.g.
         ("Key", "INT PRIMARY_KEY")
 
     """
-    entity_string = []
+    entity_list = []
     for entity_name, *entity_format in entities:
-        entity_string.append(f"[{entity_name}] {', '.join(entity_format)}")
-    entity_string = ", ".join(entity_string)
+        entity_list.append(f"[{entity_name}] {', '.join(entity_format)}")
+    entity_string = ", ".join(entity_list)
 
-    sql_string = f"CREATE TABLE IF NOT EXISTS {table_name} ({entity_string});"
+    sql_string = f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} ({entity_string});"
     sql_execute(sql_string)
 
 
-def drop_table(table_name: str) -> None:
+def drop_table() -> None:
     """Drop table if it exists."""
-    sql_string = f"DROP TABLE IF EXISTS {table_name};"
+    sql_string = f"DROP TABLE IF EXISTS {TABLE_NAME};"
     sql_execute(sql_string)
 
 
-def add_entry(table_name: str, entities: List[str], values: List[str]) -> str:
-    """Add entry to existing db table and return its id.
+def add_entry(values: List[str]) -> None:
+    """Add entry to existing db table.
 
-    :entities: List of entities for which to add values.
     :values: List of values to add.
 
     """
-    entity_string = ", ".join([f"[{entity_name}]" for entity_name in entities])
-    value_string = ", ".join([f"'{value}'" for value in values])
-
-    sql_string = f"INSERT INTO {table_name} ({entity_string}) VALUES ({value_string});"
-    entry_id = sql_execute(sql_string)
-    return entry_id
+    sql_string = f"INSERT INTO {TABLE_NAME} VALUES(?, ?);"
+    parameters = tuple(values)
+    sql_execute(sql_string, parameters)
 
 
-def get_all_entries(table_name: str) -> List[tuple]:
+def get_all_entries() -> List[tuple]:
+    """Get all entries from existing db table."""
+    sql_string = f"SELECT ROWID, * FROM {TABLE_NAME};"
+    return sql_execute(sql_string)
+
+
+def get_entry_by_id(rowid: int) -> tuple:
     """Get entry from existing db table by id."""
-    sql_string = f"SELECT ROWID, * FROM {table_name};"
-    sql_execute(sql_string)
-    with sqlite3.connect(DB_NAME) as connection:
-        cursor = connection.cursor()
-        cursor.execute(sql_string)
-    return cursor.fetchall()
+    sql_string = f"SELECT * FROM {TABLE_NAME} WHERE ROWID=?;"
+    parameters = (rowid,)
+    entry, = sql_execute(sql_string, parameters)
+    return entry
 
 
-def get_entry_by_id(table_name: str, rowid: int) -> tuple:
-    """Get entry from existing db table by id."""
-    sql_string = f"SELECT * FROM {table_name} WHERE ROWID = '{rowid}';"
-    sql_execute(sql_string)
-    with sqlite3.connect(DB_NAME) as connection:
-        cursor = connection.cursor()
-        cursor.execute(sql_string)
-    return cursor.fetchone()
-
-
-def print_table(table_name: str, entities: Optional[List[str]] = None) -> None:
-    """Print contents of db table, optionally filter by entities."""
-    entities = "*" if entities is None or len(entities) == 0 else ", ".join(entities)
-    sql_string = f"SELECT ROWID, {entities} FROM {table_name};"
-    with sqlite3.connect(DB_NAME) as connection:
-        cursor = connection.cursor()
-        cursor.execute(sql_string)
-        print(cursor.fetchall())
+def print_table() -> None:
+    """Print contents of db table with rowid."""
+    sql_string = f"SELECT ROWID, * FROM {TABLE_NAME};"
+    print(sql_execute(sql_string))
 
 
 if __name__ == "__main__":
-    drop_table(TABLE_NAME)
-    create_table(TABLE_NAME, entities=DATA)
-    add_entry(
-        TABLE_NAME,
-        entities=ENTITIES,
-        values=["test.txt", "Hello, this is some text."]
-    )
-    print_table(TABLE_NAME)
+    drop_table()
+    create_table(DATA)
+    add_entry(["test.txt", "Hello, this is some text."])
+    print_table()
