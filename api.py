ffd8477cee5280796860cfc15fd089ca680afdd3
wim-peter <wim-peter.dirks@ordina.nl>
2023-02-02 14:44:05 +0100

    Update to use nltk and clean up

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pickle
 
 from fastapi import FastAPI, UploadFile
-from nltk import tokenize
+import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 
@@ -11,9 +11,11 @@ from db import (
     add_entry,
     get_all_entries,
     get_entry_by_id,
-    ENTITIES,
-    TABLE_NAME,
 )
+
+nltk.download("words")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("maxent_ne_chunker")
 
 app = FastAPI()
 
@@ -32,9 +34,9 @@ async def upload_file(file: UploadFile) -> dict:
         contents = await file.read()
         file_name = file.filename
         data = str(contents, "utf-8")
-        add_entry(TABLE_NAME, entities=ENTITIES, values=[file_name, data])
+        add_entry([file_name, data])
     except Exception as e:
-        message = e
+        message = str(e)
     finally:
         await file.close()
 
@@ -46,7 +48,7 @@ async def upload_file(file: UploadFile) -> dict:
 
 @app.get("/file")
 def get_all_files() -> dict:
-    entries = get_all_entries(TABLE_NAME)
+    entries = get_all_entries()
 
     nr_files = len(entries)
     files = [{"id": rowid, "file_name": file_name} for rowid, file_name, _ in entries]
@@ -59,7 +61,7 @@ def get_all_files() -> dict:
 
 @app.get("/file/{file_id}")
 def get_file(file_id: int) -> dict:
-    file_name, contents = get_entry_by_id(TABLE_NAME, file_id)
+    file_name, contents = get_entry_by_id(file_id)
 
     return {
         "file_name": file_name,
@@ -69,9 +71,9 @@ def get_file(file_id: int) -> dict:
 
 @app.get("/file/{file_id}/tokens")
 def get_tokens(file_id: int) -> dict:
-    _, contents = get_entry_by_id(TABLE_NAME, file_id)
+    _, contents = get_entry_by_id(file_id)
 
-    tokens = tokenize.word_tokenize(contents)
+    tokens = nltk.tokenize.word_tokenize(contents)
     unique_tokens = sorted(set(token.lower() for token in tokens if token.isalpha()))
     token_count = len(unique_tokens)
 
@@ -83,9 +85,9 @@ def get_tokens(file_id: int) -> dict:
 
 @app.get("/file/{file_id}/sentiment")
 def get_sentiment(file_id: int) -> dict:
-    _, contents = get_entry_by_id(TABLE_NAME, file_id)
+    _, contents = get_entry_by_id(file_id)
 
-    sentences = tokenize.sent_tokenize(contents)
+    sentences = nltk.tokenize.sent_tokenize(contents)
     sentiment_analyzer = SentimentIntensityAnalyzer()
     sentiments = {s: sentiment_analyzer.polarity_scores(s) for s in sentences}
 
@@ -94,18 +96,28 @@ def get_sentiment(file_id: int) -> dict:
 
 @app.get("/file/{file_id}/named_entities")
 def get_named_entities(file_id: int) -> dict:
+    _, contents = get_entry_by_id(file_id)
 
-    # Get text from uploaded file from database.
-    _, contents = get_entry_by_id(TABLE_NAME, file_id)
+    tokens = nltk.tokenize.word_tokenize(contents)
+    tagged_tokens = nltk.pos_tag(tokens)
+    chunks = nltk.ne_chunk(tagged_tokens)
+    named_entities = [[x.leaves()[0][0], x.label()] for x in chunks if hasattr(x, "label")]
+
+    return {"named_entities": named_entities}
+
+
+@app.get("/file/{file_id}/better_named_entities")
+def competition(file_id: int) -> dict:
+    _, contents = get_entry_by_id(file_id)
 
     # Tokenize (split) the sentences in a list of sentences.
-    sentences = tokenize.sent_tokenize(contents)
+    sentences = nltk.tokenize.sent_tokenize(contents)
 
     # Tokenize each sentence into a list of words
     data = []
     for sentence in sentences:
         print(sentence)
-        words = tokenize.word_tokenize(sentence)
+        words = nltk.tokenize.word_tokenize(sentence)
         data.extend(words)
 
     # Transform list to dataframe
