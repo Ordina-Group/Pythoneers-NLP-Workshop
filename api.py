0cbfb28f269fad5b080a003aaf6c074f58610512
marielleboot <82645719+marielleboot@users.noreply.github.com>
2023-02-10 12:32:48 +0100

    Make code black, mypy and pylint compliant

--- api.py
+++ api.py
@@ -51,7 +51,7 @@ def get_all_files() -> Dict[str, Any]:
     """Get all the files."""
     entries = get_all_entries()
     nr_files = len(entries)
-    files = [{"id": rowid, "file_name": file_name} for rowid, file_name, _ in entries]
+    files = [{"id": rowid, "file_name": file_name} for rowid, file_name in entries]
 
     return {
         "nr_files": nr_files,
