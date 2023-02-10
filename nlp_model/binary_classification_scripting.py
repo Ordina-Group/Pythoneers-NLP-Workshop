1a168c3c210a0aca5f311c978982c935f5d2b39d
wim-peter <wim-peter.dirks@ordina.nl>
2023-02-10 10:53:25 +0100

    Add pipes

--- nlp_model/binary_classification_scripting.py
+++ nlp_model/binary_classification_scripting.py
@@ -10,6 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 
 
 def main():
@@ -19,8 +20,10 @@ def main():
     output_vectorizer_path = Path.cwd() / "binary_tfidf_vec.pickle"
 
     # Convert data to dataframe
-    df = pd.read_csv(input_data_path, sep=",", names=["remove", "review", "sentiment"]
-    df = df.drop(columns="remove")
+    df = pd.read_csv(input_data_path, sep=",", names=["review", "sentiment"])
+    print(df.head())
+    print(df.dtypes)
+    print(df.shape)
 
     # Adjust to allow more data to be trained
     # df = df[:180000]
@@ -28,29 +31,26 @@ def main():
     # Remove empty rows
     df.dropna(inplace=True)
 
-    # Retrieve classification features
-    x = df["review"]
-
-    # Retrieve classification labels
-    y = df["sentiment"]
-
     # Option 1
     # Define classifier and vectorizer
     cls = LogisticRegression()
     vec = TfidfVectorizer()
 
+    # Retrieve classification features
+    x = df["review"].to_numpy()
+
     # Convert to array
-    x = df["review"].tolist()
+    x2 = x.copy()
 
     # Convert to vector
     x = vec.fit_transform(x)
 
     # Retrieve classification labels
     y = df["sentiment"]
+    y2 = y.copy()
 
     # Split into train and test data. Please note, validation data is not used. It splits on the training data.
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    print(type(x_test))
 
     # Train classifier based on training data with corresponding classification labels.
     cls.fit(x_train, y_train)
@@ -67,6 +67,24 @@ def main():
     # Save classifier and corresponding vectorizer for api usage.
     pickle.dump(cls, open(output_model_path, "wb"))
     pickle.dump(vec, open(output_vectorizer_path, "wb"))
+
+    # Split into train and test data. Please note, validation data is not used. It splits on the training data.
+    x2_train, x2_test, y2_train, y2_test = train_test_split(x2, y2, test_size=0.2, random_state=42)
+
+    pipe = Pipeline([('vec', TfidfVectorizer()), ('cls', LogisticRegression())])
+
+    # The pipeline can be used as any other estimator
+    # and avoids leaking the test set into the train set
+    pipe.fit(x2_train, y2_train)
+    print(pipe.score(x2_test, y2_test))
+    y2_pred = pipe.predict(x2_test)
+
+    print(confusion_matrix(y2_test, y2_pred))
+    print(classification_report(y2_test, pipe.predict(x2_test)))
+    print(f"Accuracy: {accuracy_score(y2_test, y2_pred)}")
+
+    # Save pipe and corresponding vectorizer for api usage.
+    pickle.dump(pipe, open("binary_pipe.pickle", "wb"))
 
 
 if __name__ == "__main__":
